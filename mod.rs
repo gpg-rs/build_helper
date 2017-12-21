763d6c78d1a573f24b5241b5dc652db0c18fb1d9
John Schug <john.ips.schug@gmail.com>
2017-12-21 01:13:24 -0800

    Add libtool archive parsing

--- mod.rs
+++ mod.rs
@@ -1,93 +1,91 @@
 use std::borrow::BorrowMut;
 use std::env;
 use std::ffi::{OsStr, OsString};
-use std::fs;
-use std::path::PathBuf;
+use std::fs::{self, File};
+use std::io::{BufRead, BufReader};
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use std::result;
 use std::str;
+use std::sync::{Once, ONCE_INIT};
 
 use gcc;
 
 pub type Result<T> = result::Result<T, ()>;
 
-pub struct Config {
-    pub target: String,
-    pub host: String,
-    pub compiler: gcc::Tool,
-    pub src: PathBuf,
-    pub dst: PathBuf,
-    pub build: PathBuf,
+macro_rules! lazy {
+    ($t:ty: $e:expr) => ({
+        use std::mem::ManuallyDrop;
+        static INIT: Once = ONCE_INIT;
+        static mut VALUE: Option<ManuallyDrop<$t>> = None;
+        INIT.call_once(|| {
+            let r = $e;
+            unsafe {
+                VALUE = Some(ManuallyDrop::new(r));
+            }
+        });
+        unsafe {
+            VALUE.as_ref().unwrap()
+        }
+    });
 }
 
-impl Config {
-    pub fn new<S: AsRef<OsStr>>(name: S) -> Result<Config> {
-        let target = env::var("TARGET").unwrap();
-        let host = env::var("HOST").unwrap();
-        let compiler = gcc::Build::new().get_compiler();
-        let src = PathBuf::from(env::current_dir().unwrap()).join(name.as_ref());
-        let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-        let mut build = dst.join("build");
-        build.push(name.as_ref());
+pub fn host() -> &'static str {
+    lazy!(String: env::var("HOST").unwrap())
+}
 
-        fs::create_dir_all(&build)
-            .map_err(|e| eprintln!("unable to create build directory: {}", e))?;
+pub fn target() -> &'static str {
+    lazy!(String: env::var("TARGET").unwrap())
+}
 
-        Ok(Config {
-            target,
-            host,
-            compiler,
-            src,
-            dst,
-            build,
-        })
-    }
+pub fn out_dir() -> &'static Path {
+    lazy!(PathBuf: PathBuf::from(env::var_os("OUT_DIR").unwrap()))
+}
 
-    pub fn configure(&self) -> Result<Command> {
-        let cflags = self.compiler
-            .args()
-            .iter()
-            .fold(OsString::new(), |mut c, a| {
-                c.push(a);
-                c.push(" ");
-                c
-            });
+pub fn get_env(name: &str) -> Option<OsString> {
+    println!("cargo:rerun-if-env-changed={}", name);
+    env::var_os(name)
+}
 
-        let mut cmd = Command::new("sh");
-        cmd.current_dir(&self.build)
-            .env("CC", self.compiler.path())
-            .env("CFLAGS", &cflags)
-            .arg(msys_compatible(self.src.join("configure"))?);
-        if self.host != self.target {
-            cmd.arg("--build").arg(gnu_target(&self.host));
-            cmd.arg("--host").arg(gnu_target(&self.target));
+pub fn for_each_line<P: AsRef<Path>, F: FnMut(&str)>(path: P, mut f: F) -> Result<()> {
+    let mut file = BufReader::new(File::open(path).or(Err(()))?);
+    let mut line = String::new();
+    loop {
+        line.clear();
+        if file.read_line(&mut line).or(Err(()))? == 0 {
+            return Ok(());
         }
-        cmd.arg("--disable-dependency-tracking");
-        cmd.arg("--enable-static");
-        cmd.arg("--disable-shared");
-        cmd.arg("--with-pic");
-        cmd.arg({
-            let mut s = OsString::from("--prefix=");
-            s.push(msys_compatible(&self.dst)?);
-            s
-        });
-        Ok(cmd)
+        f(&line);
     }
+}
 
-    pub fn make(&self) -> Command {
-        let name = if cfg!(any(target_os = "freebsd", target_os = "dragonfly")) {
-            "gmake"
+pub fn run<C>(mut cmd: C) -> Result<String>
+where C: BorrowMut<Command> {
+    let cmd = cmd.borrow_mut();
+    eprintln!("running: {:?}", cmd);
+    match cmd.stdin(Stdio::null())
+        .spawn()
+        .and_then(|c| c.wait_with_output())
+    {
+        Ok(output) => if output.status.success() {
+            String::from_utf8(output.stdout).or(Err(()))
         } else {
-            "make"
-        };
-        let mut cmd = Command::new(name);
-        cmd.env_remove("DESTDIR");
-        if cfg!(windows) {
-            cmd.env_remove("MAKEFLAGS").env_remove("MFLAGS");
+            eprintln!(
+                "command did not execute successfully, got: {}",
+                output.status
+            );
+            Err(())
+        },
+        Err(e) => {
+            eprintln!("failed to execute command: {}", e);
+            Err(())
         }
-        cmd.current_dir(&self.build);
-        cmd
     }
+}
+
+pub fn output<C>(mut cmd: C) -> Result<String>
+where C: BorrowMut<Command> {
+    run(cmd.borrow_mut().stdout(Stdio::piped()))
 }
 
 pub fn msys_compatible<P: AsRef<OsStr>>(path: P) -> Result<OsString> {
@@ -122,35 +120,110 @@ pub fn gnu_target(target: &str) -> String {
     }
 }
 
-pub fn run<C>(mut cmd: C) -> Result<String>
-where
-    C: BorrowMut<Command>,
-{
-    let cmd = cmd.borrow_mut();
-    eprintln!("running: {:?}", cmd);
-    match cmd.stdin(Stdio::null())
-        .spawn()
-        .and_then(|c| c.wait_with_output())
-    {
-        Ok(output) => if output.status.success() {
-            String::from_utf8(output.stdout).or(Err(()))
-        } else {
-            eprintln!(
-                "command did not execute successfully, got: {}",
-                output.status
-            );
-            Err(())
-        },
-        Err(e) => {
-            eprintln!("failed to execute command: {}", e);
-            Err(())
+pub struct Config {
+    pub compiler: gcc::Tool,
+    pub src: PathBuf,
+    pub dst: PathBuf,
+    pub build: PathBuf,
+}
+
+impl Config {
+    pub fn new<S: AsRef<OsStr>>(name: S) -> Result<Config> {
+        let compiler = gcc::Build::new().get_compiler();
+        let src = PathBuf::from(env::current_dir().unwrap()).join(name.as_ref());
+        let dst = out_dir().to_owned();
+        let mut build = dst.join("build");
+        build.push(name.as_ref());
+
+        fs::create_dir_all(&build)
+            .map_err(|e| eprintln!("unable to create build directory: {}", e))?;
+
+        Ok(Config {
+            compiler,
+            src,
+            dst,
+            build,
+        })
+    }
+
+    pub fn configure(&self) -> Result<Command> {
+        let cflags = self.compiler
+            .args()
+            .iter()
+            .fold(OsString::new(), |mut c, a| {
+                c.push(a);
+                c.push(" ");
+                c
+            });
+
+        let mut cmd = Command::new("sh");
+        cmd.current_dir(&self.build)
+            .env("CC", self.compiler.path())
+            .env("CFLAGS", &cflags)
+            .arg(msys_compatible(self.src.join("configure"))?);
+        if host() != target() {
+            cmd.arg("--build").arg(gnu_target(host()));
+            cmd.arg("--host").arg(gnu_target(target()));
         }
+        cmd.arg("--disable-dependency-tracking");
+        cmd.arg("--enable-static");
+        cmd.arg("--disable-shared");
+        cmd.arg("--with-pic");
+        cmd.arg({
+            let mut s = OsString::from("--prefix=");
+            s.push(msys_compatible(&self.dst)?);
+            s
+        });
+        Ok(cmd)
+    }
+
+    pub fn make(&self) -> Command {
+        let name = if cfg!(any(target_os = "freebsd", target_os = "dragonfly")) {
+            "gmake"
+        } else {
+            "make"
+        };
+        let mut cmd = Command::new(name);
+        cmd.env_remove("DESTDIR");
+        if cfg!(windows) {
+            cmd.env_remove("MAKEFLAGS").env_remove("MFLAGS");
+        }
+        cmd.current_dir(&self.build);
+        cmd
     }
 }
 
-pub fn output<C>(mut cmd: C) -> Result<String>
-where
-    C: BorrowMut<Command>,
-{
-    run(cmd.borrow_mut().stdout(Stdio::piped()))
+pub fn parse_libtool_file<P: AsRef<Path>>(path: P) -> Result<()> {
+    for_each_line(path, |l| {
+        if l.trim().starts_with("dependency_libs") {
+            if let Some(s) = l.splitn(2, '=').nth(1) {
+                parse_linker_flags(s.trim().trim_matches('\''))
+            }
+        }
+    })
+}
+
+pub fn parse_linker_flags(flags: &str) {
+    let parts = flags.split(|c: char| c.is_whitespace()).filter_map(|p| {
+        if p.len() > 2 {
+            Some(p.split_at(2))
+        } else {
+            None
+        }
+    });
+
+    for (flag, val) in parts {
+        match flag {
+            "-L" => {
+                println!("cargo:rustc-link-search=native={}", val);
+            }
+            "-F" => {
+                println!("cargo:rustc-link-search=framework={}", val);
+            }
+            "-l" => {
+                println!("cargo:rustc-link-lib={}", val);
+            }
+            _ => (),
+        }
+    }
 }
